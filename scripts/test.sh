4c545584ad55d65be2d088001c98ab2bcfb54109
xiaogangjia <jia266163@gmail.com>
2025-05-03 17:35:03 +0200

    script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 python run_calvin.py --config-name=config_calvin \
-            --multirun rollout_lh_skip_epochs=9 \
-            batch_size=256 \
+            --multirun rollout_lh_skip_epochs=10 \
+            batch_size=64 \
             devices=1 \
             root_data_dir=/home/huang/david/MoDE/calvin/dataset/task_D_D
