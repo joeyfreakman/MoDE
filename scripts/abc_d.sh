962d24b1db461aea55f98cdcd6a83fd0f95d5b36
xiaogangjia <jia266163@gmail.com>
2025-05-04 18:52:28 +0200

    script

--- scripts/abc_d.sh
+++ scripts/abc_d.sh
@@ -1,6 +1,6 @@
 python run_calvin.py --config-name=config_calvin \
-            --multirun rollout_lh_skip_epochs=10 \
-            batch_size=128 \
+            --multirun rollout_lh_skip_epochs=5 \
+            batch_size=256 \
             devices=4 \
             logger.group=abc2d \
             root_data_dir=/home/huang/david/MoDE/calvin/dataset/task_ABC_D
