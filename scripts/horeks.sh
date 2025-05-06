4de9280ca5f6344920dd532f9f022c03eaba2f94
xiaogangjia <jia266163@gmail.com>
2025-05-06 23:42:42 +0200

    test

--- scripts/horeks.sh
+++ scripts/horeks.sh
@@ -1,6 +1,8 @@
 python run_calvin.py --config-name=config_calvin \
             --multirun rollout_lh_skip_epochs=19 \
-            batch_size=128 \
-            devices=4 \
-            logger.group=mode_rgb \
+            model=mode_agent_pc \
+            batch_size=32 \
+            devices=1 \
+            logger.group=mode_pc_no_lr \
+            cam_file=/home/hk-project-robolear/ll6323/nips25/MoDE/mode/utils/cam_params.pkl \
             root_data_dir=/hkfs/work/workspace/scratch/ll6323-david_dataset_2/calvin/dataset/task_ABC_D
