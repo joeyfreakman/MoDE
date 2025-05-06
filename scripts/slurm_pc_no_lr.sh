a9fb7df139bc68e814b355c0130210d9a5e44d09
xiaogangjia <jia266163@gmail.com>
2025-05-06 21:35:25 +0200

    add mode pc

--- scripts/slurm_pc_no_lr.sh
+++ scripts/slurm_pc_no_lr.sh
@@ -30,10 +30,8 @@ export TORCH_USE_CUDA_DSA=1
 
 srun python run_calvin.py --config-name=config_calvin \
             --multirun rollout_lh_skip_epochs=9 \
-            model=mode_agent_pc \
             batch_size=128 \
             devices=4 \
             logger.group=mode_pc_no_lr \
-            cam_file=/home/hk-project-robolear/ll6323/nips25/MoDE/mode/utils/cam_params.pkl \
             root_data_dir=/hkfs/work/workspace/scratch/ll6323-david_dataset_2/calvin/dataset/task_ABC_D
 # batch_size=2 model=smolflow_agent # batch_size=4 model.use_lora=False # model=vlm_berg_agent # batch_size=2 model.vla_mode='reduced_head' #model.use_perceiver=False model.use_incontext=True #seed=242 #model=mode_agent
