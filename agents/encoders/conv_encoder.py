6b5722ece704415f800f9633bc3e31d4adb4100e
xiaogangjia <jia266163@gmail.com>
2025-05-07 09:23:23 +0200

    xlstm rgb

--- agents/encoders/conv_encoder.py
+++ agents/encoders/conv_encoder.py
@@ -5,6 +5,29 @@ from .pretrained_resnets import FiLMResNet50Policy
 from .convnext import ConvNextv2, FiLMConvNeXtV2Policy
 
 
+class Conv_rgb(nn.Module):
+    def __init__(self,):
+        super(Conv_rgb, self).__init__()
+
+        # self.pc_static = ConvNextv2(pretrained=False)
+        # self.pc_gripper = ConvNextv2(pretrained=False)
+
+        self.rgb_static = FiLMResNet50Policy(condition_dim=512, pretrained=True)
+        self.rgb_gripper = FiLMResNet50Policy(condition_dim=512, pretrained=True)
+
+    # For point clouds of each camera view, first 3 dimensions stand for xyz, other 3 dimensions stand for rgb
+    def forward(self, x):
+
+        lang_emb = x['latent_goal']
+
+        static_tokens = self.rgb_static(x['rgb_static'], lang_emb)
+        gripper_tokens = self.rgb_gripper(x['rgb_gripper'], lang_emb)
+
+        cam_features = torch.stack([static_tokens, gripper_tokens], dim=1)
+
+        return cam_features
+
+
 class Conv_pc(nn.Module):
     def __init__(self,):
         super(Conv_pc, self).__init__()
