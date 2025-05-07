65c0552072b0fc769871c38e7467acc9150dddc8
xiaogang jia <xiaogangjia@xiaogangs-air.speedport.ip>
2025-05-07 23:10:30 +0200

    script

--- agents/encoders/conv_encoder.py
+++ agents/encoders/conv_encoder.py
@@ -9,23 +9,23 @@ class Conv_pc_rgb(nn.Module):
     def __init__(self, fuse_type='add'):
         super(Conv_pc_rgb, self).__init__()
 
-        # self.pc_static = ConvNextv2(pretrained=False)
-        # self.pc_gripper = ConvNextv2(pretrained=False)
+        self.pc_static = ConvNextv2(pretrained=False)
+        self.pc_gripper = ConvNextv2(pretrained=False)
 
-        self.pc_static = FiLMResNet50Policy(condition_dim=512, pretrained=True)
-        self.pc_gripper = FiLMResNet50Policy(condition_dim=512, pretrained=True)
+        # self.pc_static = FiLMResNet50Policy(condition_dim=512, pretrained=True)
+        # self.pc_gripper = FiLMResNet50Policy(condition_dim=512, pretrained=True)
 
         # Add a learnable preprocessing layer to adapt xyz to a more RGB-like distribution
-        self.preprocessing_static = nn.Sequential(
-            nn.Conv2d(3, 3, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(3),
-            nn.ReLU()
-        )
-        self.preprocessing_gripper = nn.Sequential(
-            nn.Conv2d(3, 3, kernel_size=1, stride=1, padding=0, bias=True),
-            nn.BatchNorm2d(3),
-            nn.ReLU()
-        )
+        # self.preprocessing_static = nn.Sequential(
+        #     nn.Conv2d(3, 3, kernel_size=1, stride=1, padding=0, bias=True),
+        #     nn.BatchNorm2d(3),
+        #     nn.ReLU()
+        # )
+        # self.preprocessing_gripper = nn.Sequential(
+        #     nn.Conv2d(3, 3, kernel_size=1, stride=1, padding=0, bias=True),
+        #     nn.BatchNorm2d(3),
+        #     nn.ReLU()
+        # )
 
         self.rgb_static = FiLMResNet50Policy(condition_dim=512, pretrained=True)
         self.rgb_gripper = FiLMResNet50Policy(condition_dim=512, pretrained=True)
@@ -40,11 +40,11 @@ class Conv_pc_rgb(nn.Module):
         static_tokens = self.rgb_static(x['rgb_static'], lang_emb)
         gripper_tokens = self.rgb_gripper(x['rgb_gripper'], lang_emb)
 
-        static_inputs = self.preprocessing_static(x['pc_static'])
-        gripper_inputs = self.preprocessing_gripper(x['pc_gripper'])
+        # static_inputs = self.preprocessing_static(x['pc_static'])
+        # gripper_inputs = self.preprocessing_gripper(x['pc_gripper'])
 
-        pc_static_tokens = self.pc_static(static_inputs, lang_emb)
-        pc_gripper_tokens = self.pc_gripper(gripper_inputs, lang_emb)
+        pc_static_tokens = self.pc_static(x['pc_static'])
+        pc_gripper_tokens = self.pc_gripper(x['pc_gripper'])
 
         if self.fuse_type == 'add':
 
