f167fa3b1236a22dfc8737b947be4f405ba7fe9d
xiaogangjia <jia266163@gmail.com>
2025-05-08 21:59:19 +0200

    script

---
+++ scripts/new/rgb_512_act20.sh
@@ -0,0 +1,15 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python run_calvin.py --config-name=config_calvin \
+            --multirun rollout_lh_skip_epochs=9 \
+            batch_size=128 \
+            devices=4 \
+            model=depth_agent \
+            model/obs_encoders=conv_rgb \
+            model.use_lr_scheduler=True \
+            logger.group=rgb_512_act20 \
+            obs_tokens=2 \
+            xlstm_encoder_vocab_size=14 \
+            n_embd=512 \
+            act_seq_len=20 \
+            multistep=20 \
+            cam_file=/home/huang/david/MoDE/mode/utils/cam_params.pkl \
+            root_data_dir=/home/huang/david/MoDE/calvin/dataset/task_ABC_D
