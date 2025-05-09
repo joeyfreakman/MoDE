3489738131670a7b7a62bec33dd76c35324714ff
xiaogangjia <jia266163@gmail.com>
2025-05-09 16:07:26 +0200

    add film fusion

---
+++ agents/encoders/fusion_encoder.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+import torchvision
+from .pretrained_resnets import FiLMResNet50Policy
+from .convnext import ConvNextv2, FiLMConvNeXtV2Policy
+
+
+class FiLMLayer(nn.Module):
+    def __init__(self, num_features, condition_dim):
+        super(FiLMLayer, self).__init__()
+        self.num_features = num_features
+        self.condition_dim = condition_dim
+        self.gamma = nn.Linear(condition_dim, num_features)
+        self.beta = nn.Linear(condition_dim, num_features)
+
+        # Zero initialization
+        nn.init.zeros_(self.gamma.weight)
+        nn.init.zeros_(self.gamma.bias)
+        nn.init.zeros_(self.beta.weight)
+        nn.init.zeros_(self.beta.bias)
+
+    def forward(self, x, condition):
+        gamma = self.gamma(condition)
+        beta = self.beta(condition)
+        x = (1 + gamma) * x + beta  # Using (1 + gamma) to start with identity transform
+        return x.contiguous()
+
+
+class Conv_fusion(nn.Module):
+    def __init__(self, fuse_type='add'):
+        super(Conv_fusion, self).__init__()
+
+        self.pc_static = FiLMResNet50Policy(condition_dim=512, pretrained=False)
+        self.pc_gripper = FiLMResNet50Policy(condition_dim=512, pretrained=False)
+
+        self.rgb_static = FiLMResNet50Policy(condition_dim=512, pretrained=True)
+        self.rgb_gripper = FiLMResNet50Policy(condition_dim=512, pretrained=True)
+
+        self.fuse_type = fuse_type
+
+        if fuse_type == 'pc_cond_rgb':
+            self.film_pc_static = FiLMLayer(num_features=2048, condition_dim=2048)
+            self.film_pc_gripper = FiLMLayer(num_features=2048, condition_dim=2048)
+        elif fuse_type == 'rgb_cond_pc':
+            self.film_rgb_static = FiLMLayer(num_features=2048, condition_dim=2048)
+            self.film_rgb_gripper = FiLMLayer(num_features=2048, condition_dim=2048)
+        elif fuse_type == 'cross_cond':
+            self.film_pc_static = FiLMLayer(num_features=2048, condition_dim=2048)
+            self.film_pc_gripper = FiLMLayer(num_features=2048, condition_dim=2048)
+
+            self.film_rgb_static = FiLMLayer(num_features=2048, condition_dim=2048)
+            self.film_rgb_gripper = FiLMLayer(num_features=2048, condition_dim=2048)
+    # For point clouds of each camera view, first 3 dimensions stand for xyz, other 3 dimensions stand for rgb
+    def forward(self, x):
+
+        lang_emb = x['latent_goal']
+
+        static_tokens = self.rgb_static(x['rgb_static'], lang_emb)
+        gripper_tokens = self.rgb_gripper(x['rgb_gripper'], lang_emb)
+
+        pc_static_tokens = self.pc_static(x['pc_static'], lang_emb)
+        pc_gripper_tokens = self.pc_gripper(x['pc_gripper'], lang_emb)
+
+        if self.fuse_type == 'pc_cond_rgb':
+            pc_static_tokens = self.film_pc_static(pc_static_tokens, static_tokens)
+            pc_gripper_tokens = self.film_pc_gripper(pc_gripper_tokens, gripper_tokens)
+
+            cam_features = torch.stack([static_tokens, pc_static_tokens, gripper_tokens, pc_gripper_tokens], dim=1)
+
+        elif self.fuse_type == 'rgb_cond_pc':
+            static_tokens = self.film_rgb_static(static_tokens, pc_static_tokens)
+            gripper_tokens = self.film_rgb_gripper(gripper_tokens, pc_gripper_tokens)
+
+            cam_features = torch.stack([static_tokens, pc_static_tokens, gripper_tokens, pc_gripper_tokens], dim=1)
+
+        elif self.fuse_type == 'cross_cond':
+            cond_pc_static_tokens = self.film_pc_static(pc_static_tokens, static_tokens)
+            cond_pc_gripper_tokens = self.film_pc_gripper(pc_gripper_tokens, gripper_tokens)
+
+            cond_static_tokens = self.film_rgb_static(static_tokens, pc_static_tokens)
+            cond_gripper_tokens = self.film_rgb_gripper(gripper_tokens, pc_gripper_tokens)
+
+            cam_features = torch.stack([cond_static_tokens, cond_pc_static_tokens, cond_gripper_tokens, cond_pc_gripper_tokens], dim=1)
+
+        elif self.fuse_type == 'add':
+            cam_features = torch.stack([static_tokens+pc_static_tokens, gripper_tokens+pc_gripper_tokens], dim=1)
+        elif self.fuse_type == 'cat':
+            cam_features = torch.stack([static_tokens, pc_static_tokens, gripper_tokens, pc_gripper_tokens], dim=1)
+
+        return cam_features
+
---
+++ scripts/fusion/cross_cond.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH -p accelerated
+#SBATCH -A hk-project-p0022253
+#SBATCH -J calvin
+
+# Cluster Settings
+#SBATCH -n 4       # Number of tasks
+#SBATCH -c 16  # Number of cores per task
+#SBATCH -t 200 ## 1-00:30:00 # 06:00:00 # 1-00:30:00 # 2-00:00:00
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=4
+
+
+# Define the paths for storing output and error files
+#SBATCH --output=/hkfs/work/workspace/scratch/ll6323-david_dataset_2/calvin/logs/%x_%j.out
+#SBATCH --error=/hkfs/work/workspace/scratch/ll6323-david_dataset_2/calvin/logs/%x_%j.err
+
+
+# -------------------------------
+# Activate the virtualenv / conda environment
+conda activate nips25
+
+export TORCH_USE_CUDA_DSA=1
+# NNODES=1
+# NODE_RANK=0
+# PORT=29500
+# MASTER_ADDR=127.0.0.1
+#CUDA_VISIBLE_DEVICES=0,1,2,3
+
+srun python run_calvin.py --config-name=config_calvin \
+            --multirun rollout_lh_skip_epochs=9 \
+            batch_size=128 \
+            devices=4 \
+            model=depth_agent \
+            model/obs_encoders=fusion_cross \
+            model.use_lr_scheduler=True \
+            logger.group=fusion_cross \
+            obs_tokens=4 \
+            xlstm_encoder_vocab_size=16 \
+            n_embd=512 \
+            cam_file=/home/hk-project-robolear/ll6323/nips25/MoDE/mode/utils/cam_params.pkl \
+            root_data_dir=/hkfs/work/workspace/scratch/ll6323-david_dataset_2/calvin/dataset/task_ABC_D
+# batch_size=2 model=smolflow_agent # batch_size=4 model.use_lora=False # model=vlm_berg_agent # batch_size=2 model.vla_mode='reduced_head' #model.use_perceiver=False model.use_incontext=True #seed=242 #model=mode_agent
