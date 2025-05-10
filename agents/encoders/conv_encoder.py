fad7c785e582238a5446a9fef4ab14bdeaa0b554
xiaogangjia <jia266163@gmail.com>
2025-05-10 10:08:25 +0200

    add pre

--- agents/encoders/conv_encoder.py
+++ agents/encoders/conv_encoder.py
@@ -177,3 +177,58 @@ class Convnext_pc(nn.Module):
         cam_features = torch.stack([static_tokens, gripper_tokens], dim=1)
 
         return cam_features
+
+
+# SwishGLU -- A Gated Linear Unit (GLU) with the Swish activation; always better than GELU MLP!
+class SwishGLU(nn.Module):
+    def __init__(self, in_dim: int, out_dim: int) -> None:
+        super().__init__()
+        self.act, self.project = nn.SiLU(), nn.Linear(in_dim, 2 * out_dim)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        projected, gate = self.project(x).tensor_split(2, dim=-1)
+        return projected * self.act(gate)
+
+
+class Conv_pc_rgb_pretrained(nn.Module):
+    def __init__(self, fuse_type='add'):
+        super(Conv_pc_rgb_pretrained, self).__init__()
+
+        self.pc_static = FiLMResNet50Policy(condition_dim=512, pretrained=True)
+        self.pc_gripper = FiLMResNet50Policy(condition_dim=512, pretrained=True)
+
+        # Add a learnable preprocessing layer to adapt xyz to a more RGB-like distribution
+        self.preprocessing = nn.Sequential(
+            nn.Conv2d(3, 3, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(3),
+            nn.ReLU()
+        )
+
+        self.rgb_static = FiLMResNet50Policy(condition_dim=512, pretrained=True)
+        self.rgb_gripper = FiLMResNet50Policy(condition_dim=512, pretrained=True)
+
+        self.fuse_type = fuse_type
+
+    # For point clouds of each camera view, first 3 dimensions stand for xyz, other 3 dimensions stand for rgb
+    def forward(self, x):
+
+        lang_emb = x['latent_goal']
+
+        static_tokens = self.rgb_static(x['rgb_static'], lang_emb)
+        gripper_tokens = self.rgb_gripper(x['rgb_gripper'], lang_emb)
+
+        static_inputs = self.preprocessing(x['pc_static'])
+        gripper_inputs = self.preprocessing(x['pc_gripper'])
+
+        pc_static_tokens = self.pc_static(static_inputs, lang_emb)
+        pc_gripper_tokens = self.pc_gripper(gripper_inputs, lang_emb)
+
+        if self.fuse_type == 'add':
+
+            cam_features = torch.stack([static_tokens+pc_static_tokens, gripper_tokens+pc_gripper_tokens], dim=1)
+
+        elif self.fuse_type == 'cat':
+
+            cam_features = torch.stack([static_tokens, pc_static_tokens, gripper_tokens, pc_gripper_tokens], dim=1)
+
+        return cam_features
