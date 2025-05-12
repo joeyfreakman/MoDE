64e4fbae17e252960a7f623c28aa04811962f61b
xiaogangjia <jia266163@gmail.com>
2025-05-12 10:21:55 +0200

    script

--- scripts/d2d/rgb.sh
+++ scripts/d2d/rgb.sh
@@ -1,10 +1,10 @@
 CUDA_VISIBLE_DEVICES=4,5,6,7 python run_calvin.py --config-name=config_calvin \
-            --multirun rollout_lh_skip_epochs=24 \
+            --multirun rollout_lh_skip_epochs=14 \
             batch_size=128 \
             devices=4 \
             model=depth_agent_tune \
             model/obs_encoders=convnext_rgb \
-            model.use_lr_scheduler=True \
+            model.use_lr_scheduler=False \
             logger.group=d2d_rgb \
             obs_tokens=2 \
             xlstm_encoder_vocab_size=14 \
@@ -15,12 +15,12 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 python run_calvin.py --config-name=config_calvin \
             root_data_dir=/home/huang/david/MoDE/calvin/dataset/task_D_D
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 python run_calvin.py --config-name=config_calvin \
-            --multirun rollout_lh_skip_epochs=24 \
+            --multirun rollout_lh_skip_epochs=14 \
             batch_size=128 \
             devices=4 \
             model=depth_agent_tune \
             model/obs_encoders=convnext_pc \
-            model.use_lr_scheduler=True \
+            model.use_lr_scheduler=False \
             logger.group=d2d_pc \
             obs_tokens=4 \
             xlstm_encoder_vocab_size=16 \
