d99028324b23d1014f732a0f312053f2a2fd376c
xiaogangjia <jia266163@gmail.com>
2025-05-13 19:37:06 +0200

    kcist

--- scripts/kcist/rgb_pc_cat.sh
+++ scripts/kcist/rgb_pc_cat.sh
@@ -2,14 +2,43 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 python run_calvin.py --config-name=config_calvin \
             --multirun rollout_lh_skip_epochs=9 \
             batch_size=128 \
             devices=4 \
-            model=depth_agent_tune_1 \
+            model=depth_agent_tune \
             model/obs_encoders=conv_rgb_pc_cat \
             model.use_lr_scheduler=True \
-            logger.group=kcist_adam \
+            logger.group=kcist_cat_242 \
             obs_tokens=4 \
             xlstm_encoder_vocab_size=16 \
             n_embd=512 \
-            latent_dim=768 \
+            num_sampling_steps=4 \
+            cam_file=/home/huang/david/MoDE/mode/utils/cam_params.pkl \
+            root_data_dir=/home/huang/david/MoDE/calvin/dataset/task_ABC_D
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python run_calvin.py --config-name=config_calvin \
+            --multirun rollout_lh_skip_epochs=9 \
+            batch_size=128 \
+            devices=4 \
+            model=depth_agent_tune \
+            model/obs_encoders=conv_rgb \
+            model.use_lr_scheduler=True \
+            logger.group=kcist_rgb_242 \
+            obs_tokens=2 \
+            xlstm_encoder_vocab_size=14 \
+            n_embd=512 \
+            num_sampling_steps=4 \
+            cam_file=/home/huang/david/MoDE/mode/utils/cam_params.pkl \
+            root_data_dir=/home/huang/david/MoDE/calvin/dataset/task_ABC_D
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python run_calvin.py --config-name=config_calvin \
+            --multirun rollout_lh_skip_epochs=9 \
+            batch_size=128 \
+            devices=4 \
+            model=depth_agent_tune \
+            model/obs_encoders=conv_pc \
+            model.use_lr_scheduler=True \
+            logger.group=kcist_pc_242 \
+            obs_tokens=2 \
+            xlstm_encoder_vocab_size=14 \
+            n_embd=512 \
             num_sampling_steps=4 \
             cam_file=/home/huang/david/MoDE/mode/utils/cam_params.pkl \
             root_data_dir=/home/huang/david/MoDE/calvin/dataset/task_ABC_D
